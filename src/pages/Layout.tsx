6afad2fbe7838a9d5c3103d122eca8b1a2641723
HwangYeongTak <hyt78072@naver.com>
2023-06-10 23:13:23 +0900

    과제

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -13,9 +13,11 @@ const Nav = styled.div`
   font-weight: 600;
   margin: auto;
   padding-top: 20px;
+
   a {
     color: #777;
   }
+
   .active {
     color: rgb(50, 148, 248);
   }
@@ -59,11 +61,14 @@ const Layout = () => {
     <div>
       <Profile>
         <div>
-          <ProfilePicture src="https://t1.daumcdn.net/cfile/tistory/99107A335C1EED5005" alt="프로필 사진" />
+          <ProfilePicture
+            src="https://dszw1qtcnsa5e.cloudfront.net/community/20201227/c201b556-ca02-409f-95fc-013546a19189/20200712153418.763958.gif"
+            alt="프로필 사진"
+          />
         </div>
         <div>
-          <Name>제시</Name>
-          <Description>브롤스타즈~</Description>
+          <Name>영탁</Name>
+          <Description>안녕하세요..</Description>
         </div>
       </Profile>
       <Nav>
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,19 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <div>
+        <h3>15사단 정보통신대대</h3>
+        <div>병장 만기 전역</div>
+        <div>2020.04.20 ~ 2021.10.25</div>
+
+        <h3>충남대 컴퓨터융합학부 재학중</h3>
+        <div>3학년 재학중</div>
+        <div>2019.03 ~ 현재</div>
+        <h3></h3>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
